29969922d54ee72c75c1cea3e21d1bb37a52660c
dragi-ns <54716240+dragi-ns@users.noreply.github.com>
2022-09-14 15:24:30 +0200

    Instead of replacing all text from the document, replace only the pasted (changed) part.

--- extension.js
+++ extension.js
@@ -32,6 +32,13 @@ function replaceIrregularWhitespaces(text, regex) {
   return text.replace(regex, " ");
 }
 
+function fixIndentation(text, indentationAmount) {
+  return text
+    .split("\n")
+    .map((line) => `${" ".repeat(indentationAmount)}${line}`)
+    .join("\n");
+}
+
 async function handleActiveTextDocumentChange(changeEvent, regex) {
   const clipboardText = await vscode.env.clipboard.readText();
 
@@ -50,21 +57,24 @@ async function handleActiveTextDocumentChange(changeEvent, regex) {
     ) {
       console.log("Detected irregular whitespaces! Replacing...");
 
-      // TODO: Instead of replacing all text from the document, replace only the pasted (changed) part.
-      const newText = replaceIrregularWhitespaces(document.getText(), regex);
+      let newText = replaceIrregularWhitespaces(contentChange.text, regex);
       if (!newText) {
         return;
       }
+      newText = fixIndentation(newText, contentChange.range.start.character);
 
-      // https://stackoverflow.com/questions/45203543/vs-code-extension-api-to-get-the-range-of-the-whole-text-of-a-document
-      const firstLine = editor.document.lineAt(0);
-      const lastLine = editor.document.lineAt(editor.document.lineCount - 1);
+      const newLinesCount = contentChange.text.split("\n").length;
+      const firstLine = document.lineAt(contentChange.range.start.line);
+      const lastLine = document.lineAt(
+        firstLine.range.start.line + (newLinesCount - 1)
+      );
       const textRange = new vscode.Range(
         firstLine.range.start,
         lastLine.range.end
       );
-      return editor.edit((editBuilder) =>
-        editBuilder.replace(textRange, newText)
+      const validatedTextRange = document.validateRange(textRange);
+      editor.edit((editorBuilder) =>
+        editorBuilder.replace(validatedTextRange, newText)
       );
     }
   });
